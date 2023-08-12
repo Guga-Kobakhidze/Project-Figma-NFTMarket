2690c07a7f74a85b17f71f50608b14a6c2408c81
Freek1ll3R <gugakobakhidze18@gmail.com>
2023-08-13 00:39:51 +0400

    Add new slider and Javascript

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+// for slider 1 ---> //
+
 const slider = document.querySelector(".slider");
 const leftHandle = document.querySelector(".left-handle");
 const rightHandle = document.querySelector(".right-handle");
@@ -22,4 +24,33 @@ leftHandle.addEventListener("click", () => {
 rightHandle.addEventListener("click", () => {
   sliderIndex = (sliderIndex + 1) % (numCards - 2);
   updateSliderPosition();
+});
+
+
+// for slider 2 ---> //
+
+const slider2 = document.querySelector(".slider2");
+const leftHandle2 = document.querySelector(".left-handle2");
+const rightHandle2 = document.querySelector(".right-handle2");
+
+let sliderIndex2 = 0;
+const cardWidth2 = 500;
+const numCards2 = document.querySelectorAll(".carousel_card2").length;
+
+const updateSlider2Position = () => {
+  slider2.style.transform = `translateX(-${sliderIndex2 * cardWidth}px)`;
+};
+
+leftHandle2.addEventListener("click", () => {
+  if (sliderIndex2 == 0) {
+    sliderIndex2 = (sliderIndex2 - 3 + numCards) % numCards;
+  } else {
+    sliderIndex2 = (sliderIndex2 - 1 + numCards) % numCards;
+  }
+  updateSlider2Position();
+});
+
+rightHandle2.addEventListener("click", () => {
+  sliderIndex2 = (sliderIndex2 + 1) % (numCards - 2);
+  updateSlider2Position();
 });
